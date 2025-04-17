855311b096f6efb4efe52ce9de809b009082dd55
tost74 <55083159+tost74@users.noreply.github.com>
2025-04-17 19:47:00 +0200

    Add files via upload

---
+++ graf.py
@@ -0,0 +1,42 @@
+# Breadth-first search (BFS) with graph
+
+graf = [[0, 1, 1, 0, 0, 0, 0, 0, 1],
+        [0, 0, 0, 0, 1, 1, 0, 1],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+        [0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 1],
+        [],
+        [0, 0, 0, 0, 0, 0, 0, 0, 1],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+        [],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1],
+        [],
+        [1, 0, 0, 1],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1]]
+
+
+
+def bfs(graph, queque, visited, road):
+    if len(visited) == len(graph):
+        return road
+    else:
+        first = queque[0]
+        pivot = 0
+        for i in graph[first]:
+            if i == 1:
+                queque.append(pivot)
+            pivot += 1
+        road.append(first)
+        queque.pop(0)
+        visited.add(first)
+        return bfs(graph, queque, visited, road)
+
+                
+que = [0]
+v = set()
+result = []
+
+
+w = bfs(graf, que, v, result)
+print(w)
